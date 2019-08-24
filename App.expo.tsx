fd13fcf43a417a706a8841cb9be7762ce5869963
Brent Vatne <brentvatne@gmail.com>
2019-08-23 17:35:47 -0700

    Add fallback when opened in Expo client

---
+++ App.expo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 15 }}>
+        react-native-appearance will be supported in Expo SDK 35+
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
